0353c0b73b12c61af255d290d368acf975872655
cnyiti <1540675842@qq.com>
2020-03-29 22:28:12 +0800

    init repo

---
+++ src/main/java/life/majiang/community/Community2Application.java
@@ -0,0 +1,13 @@
+package life.majiang.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Community2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Community2Application.class, args);
+    }
+
+}
