fea9f66435c5a4c02c04f51e9b077b0d99344a95
cnyiti <1540675842@qq.com>
2020-05-04 15:39:41 +0800

    第一季结束前

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -2,10 +2,10 @@ package life.majiang.community.controller;
 
 import life.majiang.community.dto.AccessTokenDto;
 import life.majiang.community.dto.GithubUser;
-import life.majiang.community.mapper.UserMapper;
 import life.majiang.community.model.User;
 import life.majiang.community.provider.GithubProvider;
 import life.majiang.community.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -18,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
+@Slf4j
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
@@ -60,6 +61,8 @@ public class AuthorizeController {
             response.addCookie(new Cookie("token",token));
             return "redirect:/";
         }else{
+            //登录失败
+            log.error("callback get github error,{}",githubUser);
             return "redirect:/";
         }
 
---
+++ target/classes/db/migration/V9__Add_comment_count_to_comment_table.sql
@@ -0,0 +1,2 @@
+alter table COMMENT
+	add comment_count INT default 0;
